4df4e2cb2516f6c8f03603c236083f248b396345
Tobreak-D <845001301@qq.com>
2016-06-21 21:31:11 +0800

    Create config.py

---
+++ Final/config.py
@@ -0,0 +1,24 @@
+#Squish的配置文件
+#-----------------------
+#请放心地按照自己的喜好修改下面的配置参量。
+
+#如果游戏太快或太慢，请修改速度参量
+#改变这些设置，以便在游戏中使用其他图像：
+qinnv_image = 'qinnv.png'
+weight_image = 'weight.png'
+splashi_image = 'weight.png'
+
+#改变这些设置以影响一般的外观：
+screen_size = 800,600
+background_color = 255,255,255
+margin = 30
+full_screen = 1
+font_size = 48
+
+#这些设置会影响游戏的变现行为：
+drop_speed = 5
+qinnv_speed = 10
+speed_increase = 1
+weights_per_level = 10
+qinnv_pad_too = 40
+qinnv_pad_side = 20
